e56c06a40ce42005cc571299465302a4dc021c0f
Molly Snyder <131561844+msnyder3@users.noreply.github.com>
2023-08-10 21:06:22 -0400

    developing out SQL

---
+++ db/query.sql
@@ -0,0 +1,4 @@
+SELECT
+  users.UserName AS name, post.UserName AS UserName
+FROM post
+JOIN UserName ON UserName = UserName.id;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,41 @@
 DROP DATABASE IF EXISTS blog_db;
 
 CREATE DATABASE blog_db;
+
+USE blog_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT NOT NULL,
+    UserName varchar(15) NOT NULL,
+    FirstName varchar(30) NOT NULL,
+    LastName varchar(30),
+    Email varchar(320) NOT NULL,
+    Password varchar(18) NOT NULL,
+
+)
+
+CREATE TABLE post (
+    id INT AUTO_INCREMENT NOT NULL,
+    UserName users_UserName,
+    Title varchar,
+    VenueName location_VenueName,
+    EventDate DATE,
+    Genre genres_GenreName,
+    PostText text(65000),
+)
+
+CREATE TABLE location (
+    id INT AUTO_INCREMENT NOT NULL,
+    VenueName varchar,
+    AddressLine1 varchar,
+    AddressLine2 varchar,
+    City varchar,
+    State varchar,
+    ZipCode varchar,
+    VenueSize INT,
+)
+
+CREATE TABLE genres (
+    id INT AUTO_INCREMENT NOT NULL,
+    GenreName varchar,
+)
