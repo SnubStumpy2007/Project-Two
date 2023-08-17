c342ea1021f1d65264b7c8e3f274877e4d32057f
Molly Snyder <131561844+msnyder3@users.noreply.github.com>
2023-08-17 15:33:06 -0400

    updated to newest version

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -6,14 +6,7 @@ const { userAccount, Post } = require('../models');
 // Use the "withAuth" middleware to protect the dashboard route.
 router.get('/', withAuth, (req, res) => {
     // Fetch data or perform actions specific to the dashboard and render the dashboard view.
-    res.render('dashboard');
+    res.render('profileDashboard');
 });
 
-router.get ('/', (req, res) => {
-    Post.findAll({
-        where: {
-            sea
-        }
-    })
-})
 module.exports = router;
